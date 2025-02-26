bbd3fef3b0e8ca871d0c7df89cb5e5aa6dfa0cd1
Kartikbhosale28 <155899556+Kartikbhosale28@users.noreply.github.com>
2025-02-26 17:21:44 +0000

    dataypes

---
+++ basic/02_datatypes.js
@@ -0,0 +1,18 @@
+"use strict";//treating all the JS code as a newer version that means we are using the newer version of js 
+//alert(3 + 3) // humlog yaha par node use kar rahe hai isliye output nahi aa raha agar humlog ne isko browser pe use kiya toh output 6 ayega
+
+// datatypes:
+// 1. number =>2 to power 53
+// 2.bigint =>when number is way bigger
+//3. string=>"" or ''
+//4. boolean=> true anf false
+//5 null => standalone value// standalone value:A standalone type is a data type that exists on its own and is not derived from another type.
+// null is a primitive type, meaning it is not an object, function, or array. It is unique because it only has one value: null itself.
+  let value= null;
+  console.log(value);
+  console.log(typeof value) // o/p => will be an object due to js bug //typeof=> type of a variable ie datatype
+  
+//6. undefined=> when we declare a variable but we dont assign any value to it
+//7.symbol=> unique
+
+//8.object
