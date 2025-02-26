2edba21ccc35a56f111832fedf3983252896ec56
Kartikbhosale28 <155899556+Kartikbhosale28@users.noreply.github.com>
2025-02-26 17:23:19 +0000

    variables

--- basic/01_variables.js
+++ basic/01_variables.js
@@ -5,7 +5,7 @@ account_City = "Lasalgaon"
 
 console.log(account_Id);
 // constant are the ones which cannot be changed once its declared it remains the same 
-//therefore account_Id=4678 not possible 
+//therefore account_Id=4678 not possible  
 
 account_Email="baburaoapte@69"
 account_Password="shyamraju"
@@ -16,8 +16,7 @@ console.table([account_Id,account_Email,account_Password,account_City])
 
 // we dont use var keyword because it does not support scope
 /* scope means ={} that means if one programmer has set the value of account_Password in one code and another programmer is
-   using the another code and if he/she uses account_Password variable and changes the vlaue or sets the other value it gets reflected
-   in the first programmers code as well.... therefore we dont use var 
+   using the another code and if he/she uses account_Password variable and changes the vlaue or sets the other value it gets reflected  in the first programmers code as well.... therefore we dont use var 
 
 */
 //we will use let keyword
